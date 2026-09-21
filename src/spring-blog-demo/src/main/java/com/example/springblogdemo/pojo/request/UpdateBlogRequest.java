8b150840d3d01e78609ef974eb2a08fef7bb8883
Rational <2760535020@qq.com>
2026-09-21 23:25:39 +0800

    博客系统笔记

--- src/spring-blog-demo/src/main/java/com/example/springblogdemo/pojo/request/UpdateBlogRequest.java
+++ src/spring-blog-demo/src/main/java/com/example/springblogdemo/pojo/request/UpdateBlogRequest.java
@@ -10,11 +10,6 @@ import lombok.Data;
 public class UpdateBlogRequest {
     @NotNull(message = "博客id不能为空")
     private Integer id;
-
-    // 刻意不加 @NotBlank 和 @NotNull：
-    // 更新博客允许「只改内容不改标题」，所以两个字段都可以为 null，
-    // 由 Service 判断是否所有字段都为空，都为空就直接返回。
     private String title;
-
     private String content;
 }
